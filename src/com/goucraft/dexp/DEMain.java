548ca1a1ce31084eaff43c3f659b06df8eaae1ae
QuickSand <775659148@qq.com>
2017-02-12 20:06:46 +0800

    增加OnEnable

--- src/com/goucraft/dexp/DEMain.java
+++ src/com/goucraft/dexp/DEMain.java
@@ -1,7 +1,17 @@
 package com.goucraft.dexp;
 
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+
 /**
  * Created by Administrator on 2017/2/12 0012.
  */
-public class DEMain {
+public class DEMain extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info(ChatColor.RED + "======================");
+        getLogger().info(ChatColor.RED + "接小中型插件定制");
+        getLogger().info(ChatColor.RED + "联系方式QQ:775659148");
+        getLogger().info(ChatColor.RED + "======================");
+    }
 }
